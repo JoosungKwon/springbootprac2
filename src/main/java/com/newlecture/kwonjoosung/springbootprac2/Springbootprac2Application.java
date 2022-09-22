11b59e0007b8449ed5b039c01e583641c2bc73eb
권주성 <99165624+JoosungKwon@users.noreply.github.com>
2022-09-22 21:45:38 +0900

    Initial commit

---
+++ src/main/java/com/newlecture/kwonjoosung/springbootprac2/Springbootprac2Application.java
@@ -0,0 +1,13 @@
+package com.newlecture.kwonjoosung.springbootprac2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootprac2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springbootprac2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/newlecture/kwonjoosung/springbootprac2/Springbootprac2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newlecture.kwonjoosung.springbootprac2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootprac2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
